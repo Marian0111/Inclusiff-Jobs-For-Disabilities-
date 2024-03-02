e6876c27c4cf021ff4c8f005de5962ff52197b3c
andre <andrei02marian@gmail.com>
2024-03-02 14:16:21 +0200

    Platform to include people with disabilities in jobs market

---
+++ inclusiff/src/Hooks/useTrainings.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+function useTrainings() {
+    const getTrainings = async () => {
+        const response = await axios.get('http://localhost:10004/trainings')
+        if (response.status === 200) {
+            return response.data
+        } else {
+            console.error("Error getting trainings!")
+        }
+    }
+
+    const getLearnerTrainings = async (userId) => {
+        const response = await axios.get(`http://localhost:10004/user/${userId}/trainings`)
+        if (response.status === 200) {
+            return response.data
+        } else {
+            console.error("Error getting learner trainings")
+        }
+    }
+
+    return { getTrainings, getLearnerTrainings };
+}
+
+export default useTrainings;
